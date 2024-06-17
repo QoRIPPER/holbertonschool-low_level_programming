e733ac0733bd4eaec52941c226e0f27c667cdd84
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-17 12:30:23 -0400

    lol

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-{
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the string
+ * followed by a newline character.
+ * Return: Always 0 (Success)
+ */
 int main(void)
+{
 puts("Programming is like building a multilingual puzzle,");
-return (0); }
+return (0);
+}
