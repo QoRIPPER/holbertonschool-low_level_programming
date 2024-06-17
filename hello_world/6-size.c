6ed94f5e6440677cf3feb5535e6c7a207764321c
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-17 13:52:18 -0400

    lol

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-printf ("sizeof int is %zu\n", sizeof(int));
-printf ("sizeof long is %zu\n", sizeof(long));
+printf("sizeof int is %zu\n", sizeof(int));
+printf("sizeof long is %zu\n", sizeof(long));
 }
