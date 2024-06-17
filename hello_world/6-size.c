21ccf5e93b51248ed39866465c075c319bf589c7
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-17 13:36:45 -0400

    lol

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+/**
+* main- prints
+* Description: prints the size of various types on the computer it is compiled and run on
+* Return: always 0
+*/
+
 int main(void)
 {
-	printf("char:%zu int:%zu short:%zu long:%zu ll:%zu float:%zu double:%zu ld:%zu ptr:%zu\n",
-	       sizeof(char), sizeof(int), sizeof(short), sizeof(long), sizeof(long long),
-	       sizeof(float), sizeof(double), sizeof(long double), sizeof(void *));
-	return (0);
+printf("char: %zu\nshort: %zu\nint: %zu\nlong: %zu\n"
+"long long: %zu\nfloat: %zu\ndouble: %zu\n"
+"long double: %zu\n",
+sizeof(char), sizeof(short), sizeof(int), sizeof(long),
+sizeof(long long), sizeof(float), sizeof(double),
+sizeof(long double));
+
+return (0);
 }
