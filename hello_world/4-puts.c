41d2b32b8e681bc9a440850089302a2d2c17929c
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-17 12:23:54 -0400

    lol

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
-
-/**
- * main - Entry point of the program
- *
- * Description: Prints the string "Programming is like building a multilingual puzzle,"
- *              followed by a newline character.
- * Return: Always 0 (Success)
- */
-int main(void)
 {
-    puts("Programming is like building a multilingual puzzle,");
-    return (0);
-}
-
+int main(void)
+puts("Programming is like building a multilingual puzzle,");
+return (0); }
