eecaf483a4f551a4c0fa814e5a15844733f1f176
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-18 15:29:30 -0400

    lol

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,20 +11,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("The number %d is ", n);
-
-	if (n > 0)
-	{
-	printf("positive\n");
-	}
-	else if (n == 0)
-	{
-	printf("zero\n");
-	}
-	else
-	{
-	printf("negative\n");
-	}
-
+	
 	return (0);
 }
