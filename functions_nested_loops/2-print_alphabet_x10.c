b68c8458827f90c9e664f566006a91b9286dc6fe
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-19 12:40:44 -0400

    lol

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - lelo
+*Description: lelo
+*Return: lelo
+*/
+void print_alphabet_x10(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+	ch = 'a';
+	for (j = 0; j < 26; j++)
+	{	
+	_putchar(ch);
+	ch++;
+}
+_putchar("\n");
+}
