423678dd363409b1a7c59d9960fe87b7f6e1c184
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-19 10:31:09 -0400

    lol

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 /**
 *main - lelo
-*Description: lelo
+*_putchar - The character to print
 *Return: lelo
+* @c: The character to print
 */
 /*function prototype*/
-int _putchar(char c);
+int _putchar(char c)
+{
 
+	return write(1, &c, 1);
+}
 int main(void)
 {
-	char message[] = "_putchar\n";
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
+	return (0);
 }
