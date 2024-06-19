c7c5b9af086f043fcc2d8ddd9cdc5d6ffcfa9ef5
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-19 14:22:59 -0400

    lol

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,26 +7,24 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-	while (n <= 98)
+	for (; n <= 98; n++)
 	{
 	printf("%d", n);
 	if (n != 98)
 	{
 	printf(", ");
 	}
-	n++;
 	}
 	}
 	else
 	{
-	while (n >= 98)
+	for (; n >= 98; n--)
 	{
 	printf("%d", n);
 	if (n != 98)
 	{
 	printf(", ");
 	}
-	n--;
 	}
 	}
 	printf("\n");
