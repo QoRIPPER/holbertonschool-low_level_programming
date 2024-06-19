939bb33a30731d98519aa087778d471f7c149453
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-19 13:05:58 -0400

    lol

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_islower - Checks for lowercase character
+ *@c: lelel
+ * Return: 1 ttttttt
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	else
+	return (0);
+}
