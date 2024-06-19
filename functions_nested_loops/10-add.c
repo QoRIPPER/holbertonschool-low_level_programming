998355664cc9e16ebb76e92056673d27fac3e7f5
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-19 14:09:14 -0400

    lol

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adds two integers and returns the result
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Result of the addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
