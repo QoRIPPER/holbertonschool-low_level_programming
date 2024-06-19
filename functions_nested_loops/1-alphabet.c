334c17d58876a5f39055095a08788cfaf38775fd
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-19 12:23:45 -0400

    lol

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_alphabet - lelo
+*Description: lelo
+*Return: lelo
+*/
+void print_alphabet(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+}
