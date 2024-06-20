98b90e3d9b13eb88f5e409e919477e641c7a4922
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-20 12:32:44 -0400

    lol

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * fizzbuzz - leleloo
+ */
+
+void fizzbuzz(void)
+{
+	for (int i = 1; i <= 100; ++i)
+	{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf("FizzBuzz ");
+	}
+	else if (i % 3 == 0)
+	{
+	printf("Fizz ");
+	}
+	else if (i % 5 == 0)
+	{
+	printf("Buzz ");
+	}
+	else
+	{
+	printf("%d ", i);
+	}
+	}
+	printf("\n");
+}
+/**
+ * main - lelelo
+ * Return: cookery
+ */
+
+int main(void)
+{
+	fizzbuzz();
+	return (0);
+}
