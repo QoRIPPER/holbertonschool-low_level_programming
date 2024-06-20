014db742a57c868f354121934fecc5a0be1acd41
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-20 12:13:37 -0400

    lol

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -5,20 +5,22 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
+	int i, j
 
-	if (n <= 0)
+		if (n <= 0)
 	{
-	_putchar('\n');
-	return;
+		_putchar('\n');
 	}
-	for (int i = 0; i < n; i++)
+	else
 	{
-	for (int j = 0; j < i; j++)
-	{
-	_putchar(' ');
-	}
-	_putchar('\\');
-	_putchar('\n');
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
