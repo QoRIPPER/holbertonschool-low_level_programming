4a6bd5548e3819361a2467a90e3f0969dbeeb676
QoRIPPER <minercraftxbox360@gmail.com>
2024-06-20 13:06:27 -0400

    lol

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -3,4 +3,24 @@
  * print_diagonal - lelo
  * @n: iiiilol
  */
+void print_diagonal(int n)
+{
+int x, y;
 
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+for (y = 0; y < x; y++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+}
